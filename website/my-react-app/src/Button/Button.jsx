cbc5b4769c134eefbb75464dd67b2da7fd837cc8
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-05-23 19:38:09 -0400

    Added button and practiced different methods of styling

---
+++ website/my-react-app/src/Button/Button.jsx
@@ -0,0 +1,16 @@
+// With this button, we are going to practice 3 methods of stying. 
+// 1. External: put it in index.css. Has been commented out there for now. 
+// 2. Modules: We make a folder only for the button. We include a special css file in that folder.
+// 3. Inline: define a js constant with styles, and pass that into html element: style={styles}
+
+import styles from './Button.module.css'
+
+function Button() {
+
+    return (
+        <button className={styles.button}>Click Me</button>
+    );
+
+}
+
+export default Button
