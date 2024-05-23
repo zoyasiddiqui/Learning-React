af74230585de432eafd9ca7121c93bfae74c353a
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-05-23 18:58:46 -0400

    Created a header file

---
+++ website/my-react-app/src/Header.jsx
@@ -0,0 +1,11 @@
+
+// We can write a combination of javascript and html
+function Header() {
+    return(
+        <header>
+            <h1>My Website</h1>
+        </header>
+    );
+}
+
+export default Header
