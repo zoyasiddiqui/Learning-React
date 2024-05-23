e5ba633e3cd0e2d37b7433a7fc38cf9548cadc7f
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-05-23 19:38:42 -0400

    Changed when adding button

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App() {
   return(
     <> 
       <Header></Header>
       <Card></Card>
+      <Button></Button>
       <Footer></Footer>
     </>
   );
