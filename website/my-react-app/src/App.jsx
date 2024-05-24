a72d17c95dd4f9fccaa0cbfe956f05b88776070e
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-05-23 22:35:16 -0400

    Added student object to website. This object uses the react props feature, which I learned to work with.

--- website/my-react-app/src/App.jsx
+++ website/my-react-app/src/App.jsx
@@ -2,12 +2,16 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Card from './Card.jsx'
 import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
   return(
     <> 
       <Header></Header>
       <Card></Card>
+      <Student name="Spongebob" age={30} isStudent={true}></Student>
+      <Student name="Patrick" age={42} isStudent={false}></Student>
+      <Student></Student>
       <Button></Button>
       <Footer></Footer>
     </>
